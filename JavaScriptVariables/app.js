a14db5558d7835be5cb613e63630a761935926b7
Jacob Seaton <jacobseaton409@gmail.com>
2022-10-24 14:49:12 -0500

    Setting Variables

---
+++ JavaScriptVariables/app.js
@@ -0,0 +1,11 @@
+var firstName = 'Jacob';
+let lastName = 'Seaton';
+
+firstName = 'John';
+lastName = 'Doe';
+
+const fullName = 'Jacob Seaton';
+
+console.log(firstName);
+console.log(lastName);
+console.log(fullName);
