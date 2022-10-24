5ac0ef1632a68911289cefdec73b36ce5a7eed7a
Jacob Seaton <jacobseaton409@gmail.com>
2022-10-24 16:04:32 -0500

    Objects and arrays practice

---
+++ JavaScriptObjects&Arrays/app.js
@@ -0,0 +1,18 @@
+let friends = ['Will', 'David', 'Paul'];
+
+
+friends.push('Jackson');
+
+let friend = {
+    name: 'Will',
+    age: 25,
+    address: {
+        address1: '123 Main St',
+        city: 'New York',
+        state: 'NY'
+    }
+};
+
+console.log(friends);
+console.log(friend);
+console.log(friend.address.state);
