2eef0c11894ae9df3b810cdea004f06ce2d73836
Jacob Seaton <jacobseaton409@gmail.com>
2022-10-24 15:26:16 -0500

    DataTypes

---
+++ JavaScriptDataTypes/app.js
@@ -0,0 +1,17 @@
+var firstName = 'Jacob';
+var age = 23;
+var isMale = true;
+var isFemale = false;
+var surname = null;
+
+var person = {
+    name: firstName,
+    age: age,
+    dob: '04/25/1999'
+};
+
+person.name;
+person.age;
+person.dob;
+
+console.log(person);
