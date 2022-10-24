a3aefde65ac6abfcaf652eba261f0725682c0109
Jacob Seaton <jacobseaton409@gmail.com>
2022-10-24 17:46:04 -0500

    JavaFunctions

---
+++ JavaScriptFunctions/app.js
@@ -0,0 +1,12 @@
+
+let sum = add(221, 598);
+let val = subtract(add(2, 3), subtract(5, 3));
+console.log(val);
+
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
